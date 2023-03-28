cc2060b57483bbb1a6cfd040b8a313441502cfa3
j0a03 <johnsupergeeks@gmail.com>
2023-03-28 18:57:06 -0300

    Seed criada

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Travel.create(
+    nome: Faker::Travel::Airport.name(size: 'large', region: 'united_states'),
+    data: Faker::Date.forward,
+    price: Faker::Number.decimal,
+    desc: Faker::Restaurant.description
+)
+
+puts Travel.last
