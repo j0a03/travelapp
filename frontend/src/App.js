a5d78acc6bff4e24f1f5071b9b44cfa0e037961a
j0a03 <johnsupergeeks@gmail.com>
2023-04-11 19:29:37 -0300

    criando o Envio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import axios from 'axios'
 import {useState,useEffect} from 'react';
 
 function App(){
+  const [travel,setTravel] = useState({
+    nome: '',
+    data: '',
+    price: 0,
+    desc: ''
+  })
   const [viagens,setViagens] = useState([]);
 
   useEffect(()=>{
@@ -10,8 +16,32 @@ function App(){
       .catch(erro=> setViagens("deu erro no getAPI: ",erro))
   },[])
 
+  const cadastrarViagem=(travel)=>{
+    axios.post("http://localhost:3001/api/v1/travels",{travel})
+      .then(res=>{
+        console.log(res.data);
+        setViagens([...viagens,res.data])
+      })
+
+ 
+  } 
+  function EnvioFormulario(e){
+    e.preventDefault()
+    cadastrarViagem(travel)
+  }
   return(
       <div>
+        <form onSubmit={EnvioFormulario}>
+          <label>Nome</label>
+          <input
+            id="nome"
+            required
+            onChange={(e)=>setTravel({...travel,nome: e.target.value})}
+            value={travel.nome}
+            placeholder='Digite o nome da viagem'
+          />
+          <button type='submit'>Cadastrar Viagem</button>
+        </form>
         {
           viagens.map(v=>
             <h1>{v.nome}</h1>
