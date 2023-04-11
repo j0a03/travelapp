d9ae34ce69397ade8ad2e7ecd1d0c1f0f8dc08fc
j0a03 <johnsupergeeks@gmail.com>
2023-04-11 19:06:53 -0300

    backend pra criar ok

--- backend/app/controllers/api/v1/travels_controller.rb
+++ backend/app/controllers/api/v1/travels_controller.rb
@@ -8,6 +8,13 @@ class Api::V1::TravelsController < ApplicationController
   end
 
   def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      render json: @travel
+    else
+      render json: {errors: @travel.errors,message: "nao foi possivel cadastrar"},status: :unprocessable_entity
+    end
+      
   end
 
   def update
@@ -15,4 +22,9 @@ class Api::V1::TravelsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def travel_params
+      params.require(:travel).permit(:nome, :data, :price, :desc)
+    end
 end
