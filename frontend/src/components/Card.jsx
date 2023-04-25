65798edba824a8f532ac04d6b4737e9823fb60f9
j0a03 <johnsupergeeks@gmail.com>
2023-04-25 18:52:30 -0300

    card css ok

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,8 +1,12 @@
 import "./Card.css"
+import {BsFillTrashFill} from 'react-icons/bs'
 function Card(props){
     return (
         <div className="card">
             <h1>{props.nome}</h1>
+            <div>
+            <BsFillTrashFill size={32}/>
+            </div>
         </div>
     )
 }
