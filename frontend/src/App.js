067a8ca4b6c37cec750289541b7f8ca87e31562a
j0a03 <johnsupergeeks@gmail.com>
2023-05-16 19:21:53 -0300

    edit dom c update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,15 @@ function App(){
   }
   const editTravel = (id,travel)=>{
     axios.put(`http://localhost:3001/api/v1/travels/${id}`,{travel})
-      .then(res=>console.log(res))
+      .then(res=>{
+        let newListaDeViagens = viagens.map ( v=>{
+          if(v.id === id){
+            return res.data
+          }
+          return v
+        })
+        setViagens(newListaDeViagens)
+      })
       .catch(erro=>console.log("erro atualizado"))
   }
   function EnvioFormulario(e){
