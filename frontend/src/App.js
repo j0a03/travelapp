a324266897d3640e680deecfa99b5594de9e6881
j0a03 <johnsupergeeks@gmail.com>
2023-05-23 18:26:11 -0300

    criando popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import {useState,useEffect} from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
+import Popup from './components/Popup';
 function App(){
   const [travel,setTravel] = useState({
     nome: '',
@@ -86,6 +87,7 @@ function App(){
             setEditID={setEditID}
           />)
         }
+        <Popup/>
         </div>
       </div>
   )
